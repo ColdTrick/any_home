5ba2c31d0b1ed312736c34483df41309777bc21b
Jerôme Bakker <jeabakker@coldtrick.com>
2023-05-17 15:38:05 +0200

    chore: updated translations

--- languages/nl.php
+++ languages/nl.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * This file was created by Translation Editor v11.1
+ * On 2023-05-17 14:26
+ */
 
-return [
-	'any_home:settings:redirect:info' => "Voer links in waar de gebruikers naar worden doorgestuurd als ze naar de homepage gaan",
-	'any_home:settings:redirect_logged_in' => "Voor aangemelde gebruikers",
-	'any_home:settings:redirect_logged_out' => "Voor afgemelde gebruikers",
-];
+return array (
+  'any_home:settings:redirect:info' => 'Voer links in waar de gebruikers naar worden doorgestuurd als ze naar de homepage gaan',
+  'any_home:settings:redirect_logged_in' => 'Voor aangemelde gebruikers',
+  'any_home:settings:redirect_logged_out' => 'Voor afgemelde gebruikers',
+);
